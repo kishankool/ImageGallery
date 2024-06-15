7f0f7915e27782d33daef6785c44bd13b8ba1487
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2024-06-15 11:03:24 +0530

    camera added

--- src/screens/Upload.js
+++ src/screens/Upload.js
@@ -1,15 +1,16 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, TouchableOpacity ,Text} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import CameraView from "../components/CameraView";
+import Button from "../components/Button";
+import ImageViewer from "../components/ImageViewer";
 
-import Button from '../components/Button';
-import ImageViewer from '../components/ImageViewer';
-
-const PlaceholderImage = require('../assets/png/background-image.png');
+const PlaceholderImage = require("../assets/png/background-image.png");
 
 export default function Upload() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [useCamera, setUseCamera] = useState(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -24,13 +25,34 @@ export default function Upload() {
     }
   };
 
-  return (
+  const toggleView = async () => {
+    if (useCamera) {
+      setUseCamera(false);
+    } else {
+      setUseCamera(true);
+    }
+  };
+
+  return useCamera ? (
+    <CameraView />
+  ) : (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button theme="primary" label="Use Camera" onPress={toggleView} />
+        <TouchableOpacity onPress={toggleView}>
+          <Text> use camera</Text>
+        </TouchableOpacity>
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
@@ -41,15 +63,15 @@ export default function Upload() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
-    flex:1, 
-    paddingTop: 58
+    flex: 1,
+    paddingTop: 58,
   },
   footerContainer: {
     flex: 1 / 3,
-    alignItems: 'center',
+    alignItems: "center",
   },
 });
